7363ba1b979e8538ea8c23a7888dd2d542cfaa4d
adrshojk <adrshojk@gmail.com>
2020-06-30 11:57:48 +0530

    Updates

---
+++ precache-manifest.ebed5132f18c0eb2d031aac181decc15.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be34dffc9a70f30ddb839a5d8efce2cc",
+    "url": "/reactTest/index.html"
+  },
+  {
+    "revision": "61b2f1fad15b33297a48",
+    "url": "/reactTest/static/css/main.c1a5cd57.chunk.css"
+  },
+  {
+    "revision": "05565affe5ac887c5746",
+    "url": "/reactTest/static/js/2.2c18ca18.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactTest/static/js/2.2c18ca18.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61b2f1fad15b33297a48",
+    "url": "/reactTest/static/js/main.67ca32f1.chunk.js"
+  },
+  {
+    "revision": "16f2a695cc76f5c89853",
+    "url": "/reactTest/static/js/runtime-main.d51da744.js"
+  }
+]);
