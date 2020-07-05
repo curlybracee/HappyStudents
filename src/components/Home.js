23953385324c5afb1226da3282a3f05e96b710d7
adrshojk <adrshojk@gmail.com>
2020-07-05 13:09:51 +0530

    google-login

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,16 +1,17 @@
 import React, { useContext } from "react";
 import { UserfContext } from "../context/userState";
-import { Login } from "./Login";
+
 
 
 export const Home = () => {
   const { isToken } = useContext(UserfContext);
 
+ 
   return (
     <div className="container">
       
         <div className="wall-container">
-        <div className=""> {!isToken ? <Login /> : null}</div>
+          
         </div>
         
       
--- src/components/header.js
+++ src/components/header.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { UserfContext } from "../context/userState";
 import { useContext } from "react";
-
+import GoogleLogin from 'react-google-login'
 export const Header = () => {
   const { userLoging, isToken } = useContext(UserfContext);
-
-  const userStatus = isToken ? "Logout" : "Login";
+  const responseGoogle=(response)=>{
+    console.log(response);
+    
+  }
 
   return (
     <div className="header-container">
@@ -21,9 +23,13 @@ export const Header = () => {
           />
         </form>
       </div>
-      <button className="loginButton" onClick={userLoging}>
-        {userStatus}
-      </button>
+      <GoogleLogin
+          clientId="230577544545-c65p7g6jqg33p8rqntbc645rn200c69h.apps.googleusercontent.com"
+          buttonText="Google Login"
+          onSuccess={responseGoogle}
+          onFailure={responseGoogle}
+          cookiePolicy={'single_host_origin'}
+          />
     </div>
   );
 };
