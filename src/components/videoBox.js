fd223605d1e58dba138d741cb5c6cff302f5c448
adrshojk <adrshojk@gmail.com>
2020-07-24 23:12:30 +0530

    deploy

---
+++ src/components/videoBox.js
@@ -0,0 +1,103 @@
+import React from 'react'
+
+const VideoBox = () => {
+    return (
+         <div className="videoBox">
+    <div className='tutView'>
+      <p>   
+        
+JavaScript Part 1          
+</p> 
+<iframe src="https://www.youtube.com/embed/pUdE5oiAQmw" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      </div>
+    <div className='tutView'>
+      <p>
+      	
+JavaScript Part 1          
+      </p> 
+    
+<iframe src="https://www.youtube.com/embed/pUdE5oiAQmw" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      </div>
+    <div className='tutView'>
+      <p>
+      	
+JavaScript Part 1          
+      </p> 
+    
+<iframe src="https://www.youtube.com/embed/pUdE5oiAQmw" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      </div>
+    <div className='tutView'>
+      <p>
+      	
+JavaScript Part 1          
+      </p> 
+    
+<iframe src="https://www.youtube.com/embed/pUdE5oiAQmw" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      </div>
+    <div className='tutView'>
+      <p>
+      	
+JavaScript Part 1          
+      </p> 
+    
+<iframe src="https://www.youtube.com/embed/pUdE5oiAQmw" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      </div>
+    <div className='tutView'>
+      <p>
+      	
+JavaScript Part 1          
+      </p> 
+    
+<iframe src="https://www.youtube.com/embed/pUdE5oiAQmw" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      </div>
+    <div className='tutView'>
+      <p>
+      	
+JavaScript Part 1          
+      </p> 
+    
+<iframe src="https://www.youtube.com/embed/pUdE5oiAQmw" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      </div>
+    <div className='tutView'>
+      <p>
+      	
+JavaScript Part 1          
+      </p> 
+    
+<iframe src="https://www.youtube.com/embed/pUdE5oiAQmw" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      </div>
+    <div className='tutView'>
+      <p>
+      	
+JavaScript Part 1          
+      </p> 
+    
+<iframe src="https://www.youtube.com/embed/pUdE5oiAQmw" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      </div>
+    <div className='tutView'>
+      <p>
+      	
+JavaScript Part 1          
+      </p> 
+    
+<iframe src="https://www.youtube.com/embed/pUdE5oiAQmw" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      </div>
+    <div className='tutView'>
+      <p>
+      	
+JavaScript Part 1          
+      </p> 
+<iframe src="https://www.youtube.com/embed/pUdE5oiAQmw" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      </div>
+    <div className='tutView'>
+      <p>
+      	
+JavaScript Part 1          
+      </p> 
+<iframe src="https://www.youtube.com/embed/pUdE5oiAQmw" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      </div>
+  </div>
+    )
+}
+
+export default VideoBox
